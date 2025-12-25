edf89c33d4cca48c846702cc9b995e78ab88f3bb
thanhthien4122k3 <vtthien2003@gmail.com>
2025-12-25 15:46:47 +0700

    Initial commit

---
+++ auth-service/src/main/java/com/example/auth/dto/RegisterRequest.java
@@ -0,0 +1,6 @@
+package com.example.auth.dto;
+
+public class RegisterRequest {
+    public String email;
+    public String password;
+}
---
+++ auth-service/src/main/java/com/example/auth/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.auth.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String role;
+
+}
