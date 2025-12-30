dbf9c8f32a924e527662651840ba85ccea7f2689
Thanh Thiên <111649427+thanhthien4122k3@users.noreply.github.com>
2025-12-30 15:50:24 +0700

    Delete auth-service directory

--- auth-service/src/main/java/com/example/auth/dto/LoginRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.auth.dto;
-
-public class LoginRequest {
-    public String email;
-    public String password;
-}
--- auth-service/src/main/java/com/example/auth/dto/RegisterRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.auth.dto;
-
-public class RegisterRequest {
-    public String email;
-    public String password;
-}
